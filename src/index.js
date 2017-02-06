3465dbcf52b543eff7a1de6546fde09d52964ce2
Leonid Logvinov <logvinov.leon@gmail.com>
2017-02-06 17:13:06 +0100

    Do not require provider if web3 is already there

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-var createWeb3 = require('./provider-factory');
-
 function polyfill(env) {
     return function (node_url, wallet_provider) {
         if (env.web3 === undefined) {
+            var createWeb3 = require('./provider-factory');
             env.web3 = createWeb3(node_url, wallet_provider);
         }
     }
